6c800dad9e95b57116a439610143b4996e9d8ada
asl-marc <asl-marc@github.com>
2013-12-29 01:50:43 +0100

    added gimp, vitrualbox, vagrant

--- ubuntu-raring.sh
+++ ubuntu-raring.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 # Ubuntu 13.04 Raring - apt-get-mgmt
 
-sudo apt-add-repository ppa:diesch/testing
+# additional repositories
+# sudo apt-add-repository ppa:diesch/testing
 sudo apt-get install classicmenu-indicator
+sudo apt-get install gimp
+sudo apt-get install virtualbox
+sudo apt-get install vagrant
